bd675e7d22a493d44f9eafc07e0a3af0dedec096
smysloff <kokoc.smyslov@yandex.ru>
2026-06-15 15:13:42 +0300

    feat: add mov command

--- src/main.c
+++ src/main.c
@@ -24,10 +24,11 @@ const char *get_state_file(void);
 
 void usage(const char *app);
 void display(void);
-void process(const char *app, const char *cmd, const char *value);
+void process(const char *app, const char *cmd, const char *val1, const char *val2);
 void cmd_new(void);
 void cmd_add(const char *value);
 void cmd_del(const char *value);
+void cmd_mov(const char *val1, const char *val2);
 
 
 int
@@ -38,10 +39,13 @@ main(int argc, char *argv[])
     case 1: display();
       break;
 
-    case 2: process(argv[0], argv[1], NULL);
+    case 2: process(argv[0], argv[1], NULL, NULL);
       break;
 
-    case 3: process(argv[0], argv[1], argv[2]);
+    case 3: process(argv[0], argv[1], argv[2], NULL);
+      break;
+
+    case 4: process(argv[0], argv[1], argv[2], argv[3]);
       break;
 
     default: usage(argv[0]);
@@ -75,6 +79,7 @@ usage(const char *app)
   Fprintf(stdout, "%s new - create a new list\n", app);
   Fprintf(stdout, "%s add <value> - add a new item\n", app);
   Fprintf(stdout, "%s del <index> - delete an item by its index\n", app);
+  Fprintf(stdout, "%s mov <from> <to> - move item from index to index\n", app);
   Fprintf(stdout, "%s help - show this message\n", app);
 }
 
@@ -94,19 +99,25 @@ display(void)
 }
 
 void
-process(const char *app, const char *cmd, const char *value)
+process(const char *app, const char *cmd, const char *val1, const char *val2)
 {
-  if (!strcmp(cmd, "help") && !value)
+  if (!strcmp(cmd, "help") && !val1 && !val2)
     return usage(app);
 
-  if (!strcmp(cmd, "new") && !value)
+  if (!strcmp(cmd, "--help") && !val1 && !val2)
+    return usage(app);
+
+  if (!strcmp(cmd, "new") && !val1 && !val2)
     return cmd_new();
 
-  if (!strcmp(cmd, "add") && value)
-    return cmd_add(value);
+  if (!strcmp(cmd, "add") && val1 && !val2)
+    return cmd_add(val1);
 
-  if (!strcmp(cmd, "del") && value)
-    return cmd_del(value);
+  if (!strcmp(cmd, "del") && val1 && !val2)
+    return cmd_del(val1);
+
+  if (!strcmp(cmd, "mov") && val1 && val2)
+    return cmd_mov(val1, val2);
 
   usage(app);
 }
@@ -129,6 +140,81 @@ cmd_add(const char *value)
 }
 
 void
+cmd_mov(const char *val1, const char *val2)
+{
+  FILE *stream;
+  long long from, to, i;
+  char *endptr;
+  list_t list = NULL;
+  list_t movable = NULL;
+  node_t *node = NULL;
+  char buf[BUF_SIZE];
+
+
+  from = Strtoll(val1, &endptr, 10);
+  if (from <= 0 || from > INT_MAX)
+    die("Strtoll()");
+
+  to = Strtoll(val2, &endptr, 10);
+  if (to <= 0 || to > INT_MAX)
+    die("Strtoll()");
+
+
+  // read from the file
+
+  stream = Fopen(get_state_file(), "r");
+
+  for (i = 1; fgets(buf, sizeof(buf) - 1, stream); ++i) {
+
+    if (i == from) {
+      if (!(list_push(&movable, buf))) {
+        die("list_push()");
+      }
+    }
+
+    else if (!list_push(&list, buf)) {
+      die("list_push()");
+    }
+  }
+
+  Fclose(stream);
+
+
+  // write to the file
+
+  stream = Fopen(get_state_file(), "w");
+
+  for (i = 1; list; ++i) {
+
+    if (to == i) {
+      Fputs(movable->value, stream);
+      free(movable->value);
+      free(movable);
+    }
+
+    else {
+      Fputs(list->value, stream);
+      node = list;
+      list = list->next;
+      free(node->value);
+      free(node);
+    }
+
+  }
+
+  if (to >= i) {
+    Fputs(movable->value, stream);
+    free(movable->value);
+    free(movable);
+  }
+
+  Fclose(stream);
+
+
+  display();
+}
+
+void
 cmd_del(const char *value)
 {
   FILE *stream;
@@ -138,9 +224,10 @@ cmd_del(const char *value)
   char *endptr;
   long long n;
 
+
   // read file to linked list
 
-  stream = Fopen(get_state_file(), "r+");
+  stream = Fopen(get_state_file(), "r");
 
   n = Strtoll(value, &endptr, 10);
   if (n <= 0 || n > INT_MAX)
